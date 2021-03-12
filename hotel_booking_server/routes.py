496e6e4b0fbc8fa7f05535ba4659e8441978ecdb
Chris Vettese <chris.vettese2@gmail.com>
2021-03-12 04:26:13 -0500

    Updated endpoint

--- hotel_booking_server/routes.py
+++ hotel_booking_server/routes.py
@@ -5,22 +5,20 @@ from psycopg2.extras import DictCursor
 
 
 def add_routes(app, conn):
-    @app.route('/customers')
-    def get_all_customers():
-        query = 'SELECT * FROM hotel.customer c'
-        sin = request.args.get('sin')
-        if sin is not None:
-            query += ' WHERE c.customer_SIN = \'{}\''.format(sin)
-        query += ' LIMIT 10'
-
-        response = get_results(query, conn)
+    @app.route('/customers/<id>')
+    def get_customer(id):
+        query = 'SELECT * FROM hotel.customer c WHERE c.customer_sin = \'{}\''.format(id)
+        # sin = request.args.get('sin')
+        response = get_results(query, conn, single=True)
         return Response(response, status=200, mimetype='application/json')
 
 
-def get_results(query, conn):
+def get_results(query, conn, single):
     with conn:
         with conn.cursor() as curs:
             curs.execute(query)
             results = [dict((curs.description[i][0], value)
                             for i, value in enumerate(row)) for row in curs.fetchall()]
+            if single:
+                results = results[0]
             return json.dumps(results)
